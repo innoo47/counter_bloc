c1d7d5e3c39dc195dcb0d95d6524964ed69d1686
Inu <i0n4h0o7@gmail.com>
2025-02-21 14:52:50 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/inu/example/counter/counter_bloc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.inu.example.counter.counter_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/blocs/counter_bloc.dart
@@ -0,0 +1,10 @@
+import 'package:counter_bloc/events/counter_event.dart';
+import 'package:counter_bloc/states/counter_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterState(0)) {
+    on<Increment>((event, emit) => emit(CounterState(state.count + 1)));
+    on<Decrement>((event, emit) => emit(CounterState(state.count - 1)));
+  }
+}
